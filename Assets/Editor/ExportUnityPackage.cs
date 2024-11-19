c2728a049555bdb792546d01b66ba22c2b51358e
Ivan Lysov <ivan.lysov@gmail.com>
2024-11-19 20:14:49 +0500

    Update ExportUnityPackage.cs

--- Assets/Editor/ExportUnityPackage.cs
+++ Assets/Editor/ExportUnityPackage.cs
@@ -8,38 +8,34 @@ public class ExportUnityPackage
     [MenuItem("Tools/Export All Assets")]
     public static string ExportAllAssets()
     {
-        string packageName = "playgama-bridge";
-
-        // Define the file path for the exported package
-        string exportPath = Path.Combine(Application.dataPath, "../build/WebGL", $"{packageName}.unitypackage");
-
-        var dir = new FileInfo(exportPath).Directory;
-            if (dir != null && !dir.Exists) {
-                dir.Create();
-            }
- 
-        // Get all asset paths in the Assets folder
-        string[] allAssetPaths = AssetDatabase.GetAllAssetPaths();
- 
-        // Filter paths to include only those that are under the "Assets" folder
-       var assetsToExport = allAssetPaths.Where(path => path.StartsWith("Assets/") && !path.Equals("Assets")).ToArray();
-
-        Debug.Log($"################## AssetDatabase.ExportPackage BEGIN");
-        // Export all assets as a Unity package
-        AssetDatabase.ExportPackage(
-            assetsToExport, 
-            exportPath, 
-            ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
-       Debug.Log($"################## AssetDatabase.ExportPackage END");
- 
-        var fullpath = Path.GetFullPath(exportPath);
-
-        FileInfo fileInfo = new FileInfo("./README.md");
-        long fileSize = fileInfo.Length; // File size in bytes
-        Debug.Log($"File: README.md, Size: {fileSize} bytes");
-
-        Debug.Log($"Path is: {fullpath}");
-
-        return fullpath;
+        // The name of the unitypackage to output.
+      		const string k_PackageName = "playgama-bridge";
+      
+      		// The path to the package under the `Assets/` folder.
+      		const string k_PackagePath = "Editor";
+      
+      		// Path to export to.
+      		const string k_ExportPath = "Build";
+      
+      		public static void Export () {
+      			ExportPackage($"{k_ExportPath}/{k_PackageName}.unitypackage");
+      		}
+      
+      		public static string ExportPackage (string exportPath) {
+      			// Ensure export path.
+      			var dir = new FileInfo(exportPath).Directory;
+      			if (dir != null && !dir.Exists) {
+      				dir.Create();
+      			}
+      
+      			// Export
+      			AssetDatabase.ExportPackage(
+      				$"Assets/{k_PackagePath}",
+      				exportPath,
+      				ExportPackageOptions.Recurse
+      			);
+      
+      			return Path.GetFullPath(exportPath);
+      		}
     }
 }
