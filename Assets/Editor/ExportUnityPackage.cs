47697718616024ede8ce35e0e886341e5a9fdad4
Ivan Lysov <ivan.lysov@gmail.com>
2024-11-19 20:27:55 +0500

    Update ExportUnityPackage.cs

--- Assets/Editor/ExportUnityPackage.cs
+++ Assets/Editor/ExportUnityPackage.cs
@@ -4,38 +4,35 @@ using UnityEditor;
 using UnityEngine;
  
 public class ExportUnityPackage
-{
-    [MenuItem("Tools/Export All Assets")]
-    public static string ExportAllAssets()
-    {
-        // The name of the unitypackage to output.
-      		const string k_PackageName = "playgama-bridge";
-      
-      		// The path to the package under the `Assets/` folder.
-      		const string k_PackagePath = "Editor";
-      
-      		// Path to export to.
-      		const string k_ExportPath = "Build";
-      
-      		public static void Export () {
-      			ExportPackage($"{k_ExportPath}/{k_PackageName}.unitypackage");
-      		}
-      
-      		public static string ExportPackage (string exportPath) {
-      			// Ensure export path.
-      			var dir = new FileInfo(exportPath).Directory;
-      			if (dir != null && !dir.Exists) {
-      				dir.Create();
-      			}
-      
-      			// Export
-      			AssetDatabase.ExportPackage(
-      				$"Assets/{k_PackagePath}",
-      				exportPath,
-      				ExportPackageOptions.Recurse
-      			);
-      
-      			return Path.GetFullPath(exportPath);
-      		}
+{  
+   // The name of the unitypackage to output.
+   const string k_PackageName = "playgama-bridge";
+ 
+   // The path to the package under the `Assets/` folder.
+   const string k_PackagePath = "Editor";
+ 
+   // Path to export to.
+   const string k_ExportPath = "Build";
+
+   [MenuItem("Tools/Export All Assets")]
+   public static void ExportAllAssets () {
+    ExportPackage($"{k_ExportPath}/{k_PackageName}.unitypackage");
+   }
+ 
+   public static string ExportPackage (string exportPath) {
+    // Ensure export path.
+    var dir = new FileInfo(exportPath).Directory;
+    if (dir != null && !dir.Exists) {
+     dir.Create();
     }
+ 
+    // Export
+    AssetDatabase.ExportPackage(
+     $"Assets/{k_PackagePath}",
+     exportPath,
+     ExportPackageOptions.Recurse
+    );
+ 
+    return Path.GetFullPath(exportPath);
+   }
 }
